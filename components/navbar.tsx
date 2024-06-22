4245b21587ca00fbc965711e1c696de77a6bc6cd
Dũng Hoàng <dunghoang879@gmail.com>
2024-06-22 19:48:37 +0700

    final

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <img src="http://mba.com.vn/vnt_upload/weblink/logo.png" alt="" />
+            <img src="http://mba.com.vn/vnt_upload/weblink/logo.png" alt="MBA" />
           </Link>
           <MainNav data={categories}></MainNav>
           <NavbarActions></NavbarActions>
